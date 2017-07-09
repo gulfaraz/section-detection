4739740dcc604abe3be46b7f2bcace44c7aa8cb4
Gulfaraz Yasin <gulfarazyasin@gmail.com>
2017-07-09 23:47:18 +0530

    let there be magic

--- magic.py
+++ magic.py
@@ -2,15 +2,20 @@ import pandas
 import features
 import classify
 import cluster
-from itertools import groupby
+import itertools
 
-features.extractFeatures(inputFile="input.log", outputFile="features.csv")
+INPUT_FILE = "input.log"
+FEATURES_FILE = "features.csv"
+LABEL_COLUMN_NAME = "label"
+LINE_NUMBER_COLUMN_NAME = "line_number"
 
-unlabelledSet = pandas.read_csv("features.csv", skipinitialspace=True, header=0)
+features.extractFeatures(inputFile=INPUT_FILE, outputFile=FEATURES_FILE)
 
-unlabelledSet["label"] = pandas.DataFrame(classify.predict(unlabelledSet), columns=["label"])
+unlabelledSet = pandas.read_csv(FEATURES_FILE, skipinitialspace=True, header=0)
 
-#unlabelledSet["label"] = pandas.read_csv("labels.csv", skipinitialspace=True, header=0)
+unlabelledSet[LABEL_COLUMN_NAME] = pandas.DataFrame(classify.predict(unlabelledSet), columns=[LABEL_COLUMN_NAME])
+
+#unlabelledSet[LABEL_COLUMN_NAME] = pandas.read_csv("labels.csv", skipinitialspace=True, header=0)
 
 labelledSet = unlabelledSet
 
@@ -34,13 +39,45 @@ pattern100 = cluster.kMeans(100, patternSet, columnPrefix="pattern")
 
 resultSet = pandas.concat([pattern100, patternSet], axis=1)
 
-resultSet["line_number"] = resultSet.index
+resultSet[LINE_NUMBER_COLUMN_NAME] = resultSet.index
 
-print "Sections are probably at:"
-for k, l in groupby(resultSet.iterrows(), key=lambda x: x[1]["pattern_100"]):
-    consecutiveLines = [(t[1]["line_number"], t[1]["label"]) for t in l]
+interestingIndexes = []
+
+for k, l in itertools.groupby(resultSet.iterrows(), key=lambda x: x[1]["pattern_100"]):
+    consecutiveLines = [(t[1][LINE_NUMBER_COLUMN_NAME], t[1][LABEL_COLUMN_NAME]) for t in l]
     if len(consecutiveLines) > (offsetValue / 3):
         if consecutiveLines[0][1] > 0:
-            print consecutiveLines[0]
+            interestingIndexes.append(consecutiveLines[0])
 
 resultSet.to_csv("clusterResult.csv", sep=",", encoding="utf-8", index=True, index_label="index")
+
+def findSegmentTerminals(lineNumber, lineType):
+    startIndex = lineNumber
+    stopIndex = lineNumber
+    while (resultSet.loc[resultSet[LINE_NUMBER_COLUMN_NAME] == startIndex - 1, LABEL_COLUMN_NAME].values[0] == lineType):
+        startIndex -= 1
+    while (resultSet.loc[resultSet[LINE_NUMBER_COLUMN_NAME] == stopIndex + 1, LABEL_COLUMN_NAME].values[0] == lineType):
+        stopIndex += 1
+    return (startIndex, stopIndex)
+
+def getLinesFromFile(lineRange):
+    lineList = []
+    with open(INPUT_FILE, "r") as logFile:
+        for line in itertools.islice(logFile, lineRange[0], lineRange[1] + 1):
+            lineList.append(line)
+    return "".join(lineList)
+
+f = open("detectedSections.log", "a")
+for i, j in interestingIndexes:
+    lineRange = findSegmentTerminals(i, j)
+    if j == 1:
+        f.write("NV PAIR : " + str(lineRange[0]) + " - " + str(lineRange[1]) + "\n")
+        f.write("=======\n")
+        f.write(getLinesFromFile(lineRange))
+        f.write("\n")
+    elif j == 2:
+        f.write("ALIGNED BASIC : " + str(lineRange[0]) + " - " + str(lineRange[1]) + "\n")
+        f.write("=============\n")
+        f.write(getLinesFromFile(lineRange))
+        f.write("\n")
+f.close()
